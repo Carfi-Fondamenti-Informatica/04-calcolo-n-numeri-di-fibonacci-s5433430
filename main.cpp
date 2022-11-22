8b2a706e93952d27cfbfdb30f069076335002f18
s5433430 <115527969+s5433430@users.noreply.github.com>
2022-11-22 12:54:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int fibonacci (int n) {
    int a=0, b = 1, c = 0;
    cout << 1 << endl;
-   for (int i = 2; i =< n; i++) {
+   for (int i = 2; i <= n; i++) {
       c = a + b;
       a = b;
       b = c;
@@ -17,7 +17,7 @@ int main () {
    if (n < 2) {
       cout << "errore" << endl;
    } else {
-      fibonacci (n)
+      fibonacci(n);
     }
    return 0;
 }
